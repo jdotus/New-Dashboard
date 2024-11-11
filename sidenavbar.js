ab194bce3ef38b5b0dc7632dcd0133131e32ff1c
John David Cabal <jdcabal@otusinc.com>
2024-11-11 15:11:03 +0800

    Fixing side navigation bar

--- sidenavbar.js
+++ sidenavbar.js
@@ -11,3 +11,18 @@ function openNav() {
   function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+
+  window.onclick = function (e) {
+
+  if (!e.target.closest('.dropbtn')) { // Check if clicked outside the dropdown
+  
+    var myDropdown = document.getElementById("myDropdown");
+
+    if (myDropdown.classList.contains('show')) {
+      document.getElementById("down").style.display = "none";
+      document.getElementById("up").style.display = "inherit";
+
+      myDropdown.classList.remove('show');
+    }
+  }
+}
